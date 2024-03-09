12ed059437fd8a91649abbe31cd837ee69902790
nandonyata <nandonp02@gmail.com>
2024-03-09 12:44:12 +0700

    feat: create customer

--- customer.go
+++ customer.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"sabio-ekuator/config"
 	"sabio-ekuator/pb"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (s *Server) CreateCustomer(_ context.Context, req *pb.Customer) (*pb.CustomerMsg, error) {
+	query := `
+		INSERT INTO Customer (name, email)
+		VALUES ($1, $2)
+	`
+
+	if req.Email == "" || req.Name == "" {
+		return nil, status.Error(codes.Canceled, "Fill All Field")
+	}
+
+	_, err := config.DB.Exec(query, req.Name, req.Email)
+	if err != nil {
+		return nil, status.Error(codes.Internal, fmt.Sprintf("Err: %v\n", err))
+	}
 
 	return &pb.CustomerMsg{
 		Message: "Success create customer",
