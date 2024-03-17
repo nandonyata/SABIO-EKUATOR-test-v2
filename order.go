63cc3346719e5b79e3d32212d193b058614e27fa
nandonyata <nandonp02@gmail.com>
2024-03-17 13:33:32 +0700

    feat: create order

--- order.go
+++ order.go
@@ -2,11 +2,84 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
+	"sabio-ekuator/config"
+	"sabio-ekuator/entity"
 	"sabio-ekuator/pb"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (s *Server) CreateOrder(_ context.Context, req *pb.Order) (*pb.OrderMessage, error) {
-	return &pb.OrderMessage{}, nil
+
+	if len(req.ProductId) == 0 {
+		return nil, status.Error(codes.Aborted, "productId required")
+	}
+	if len(req.ProductId) > 1 {
+		return nil, status.Error(codes.Aborted, "Can only send 1 product id") // bcs the model order only saves 1 productId
+	}
+	if req.CustomerId == nil {
+		return nil, status.Error(codes.InvalidArgument, "customerId required")
+	}
+
+	var customerID int
+	row := config.DB.QueryRow(`
+		SELECT id FROM Customer
+		WHERE id = $1
+	`, req.CustomerId.Id)
+
+	if err := row.Scan(&customerID); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, status.Error(codes.NotFound, "Customer not found")
+		}
+
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	var product entity.Product
+	rowProduct := config.DB.QueryRow(`
+		SELECT id, price, stock FROM Product
+		WHERE id = $1
+	`, req.ProductId[0].Id)
+	if err := rowProduct.Scan(&product.Id, &product.Price, &product.Stock); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, status.Error(codes.NotFound, "Product not found")
+		}
+
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	resultStock := product.Stock - req.Quantity
+	if resultStock < 0 {
+		return nil, status.Error(codes.Aborted, "Stock not available")
+	}
+
+	totalPrice := req.Quantity * int32(product.Price)
+	_, err := config.DB.Exec(`
+		INSERT INTO "order" (customer_id, product_id, quantity, total)
+		VALUES ($1, $2, $3, $4)
+	`, customerID, product.Id, req.Quantity, totalPrice)
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, "Internal server error")
+	}
+
+	_, err = config.DB.Exec(`
+		UPDATE Product
+		SET stock = $1
+		WHERE id = $2
+	`, resultStock, product.Id)
+
+	if err != nil {
+		fmt.Println(err, "PPPP")
+		return nil, status.Error(codes.Internal, "Internal server error")
+	}
+
+	return &pb.OrderMessage{
+		Message: "Success create order",
+	}, nil
 }
 
 func (s *Server) FetchOneOrder(_ context.Context, req *pb.OrderId) (*pb.Order, error) {
